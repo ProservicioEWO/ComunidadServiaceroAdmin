45d1252e36e25d11bf375bde998a2303b8397812
daniel.ibarra <daniel.ibarra@proservicio.com>
2023-05-28 15:06:45 -0600

    Actualización de constantes

--- src/shared/cs-constants.ts
+++ src/shared/cs-constants.ts
@@ -1,5 +1,5 @@
 export const VIEW_PADDING = 4
 export const BORDER = "1px solid #E2E8F0"
-export const BASE_URL_IMG = "http://localhost:4566/cs-resources"
+export const BASE_URL_IMG = "http://cs-resources.s3.us-east-1.localhost.localstack.cloud:4566"
 export const BASE_URL_API = "http://127.0.0.1:3000"
 export const ALWAYS_TRUE = () => true
