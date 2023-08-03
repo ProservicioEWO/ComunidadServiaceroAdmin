2290f1e51b894a195b8ecdd86af4e649a269a3db
daniel.ibarra <daniel.ibarra@proservicio.com>
2023-08-03 10:36:55 -0600

    test build

--- src/shared/cs-constants.ts
+++ src/shared/cs-constants.ts
@@ -2,6 +2,5 @@ export const VIEW_PADDING = 4
 export const BORDER = "1px solid #E2E8F0"
 export const BASE_URL_IMG = "https://cs-static-res.s3.amazonaws.com/_public/images"
 export const BASE_URL_IMG_CDN = "https://d32ada10zh60nl.cloudfront.net/images"
-//export const BASE_URL_API = "http://10.4.10.183:3000"
 export const BASE_URL_API = "https://njvggk237a.execute-api.us-east-1.amazonaws.com"
 export const ALWAYS_TRUE = () => true
