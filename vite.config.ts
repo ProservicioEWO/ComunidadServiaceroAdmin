abb1340b7d5d52c1eb01018bd278297aa5cfbe7f
daniel.ibarra <daniel.ibarra@proservicio.com>
2023-08-03 12:26:18 -0600

    changed build outputdir

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react()], 
-  build: {
-    outDir: './'
-  },
+  plugins: [react()],
   resolve: {
     alias: [
       {
