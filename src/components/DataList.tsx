fd3629353bdb119e3e2a8e40fa82963cc489cd10
oscar.ramirez <oscar.ramirez@proservicio.com>
2023-09-07 17:39:26 -0600

    Ordena de forma alfabetica los usuarios.

--- src/components/DataList.tsx
+++ src/components/DataList.tsx
@@ -1,17 +1,31 @@
-import { ReactNode, useMemo, useState } from 'react'
-import { CSNextIcon, CSPreviousIcon, CSSearchUser } from '../icons/CSIcons'
-import { ALWAYS_TRUE, BORDER } from '../shared/cs-constants'
-import DataListItem from './DataListItem'
+import DataListItem from './DataListItem';
+import useAppContext from '../hooks/useAppContext';
+import { AiOutlineSortAscending, AiOutlineSortDescending } from 'react-icons/ai';
+import { ALWAYS_TRUE, BORDER } from '../shared/cs-constants';
 import {
-  List, ListProps, Skeleton,
-  VStack, HStack,
-  Input, InputGroup, InputLeftAddon,
-  ButtonGroup, Button, IconButton, Select, Text, Flex
-} from '@chakra-ui/react'
-import { InputChangeEvent } from '../shared/typeAlias'
-import { clamp } from '../shared/utils'
-import { WarningTwoIcon } from '@chakra-ui/icons'
-import useAppContext from '../hooks/useAppContext'
+  Button,
+  ButtonGroup,
+  Flex,
+  HStack,
+  Icon,
+  IconButton,
+  Input,
+  InputGroup,
+  InputLeftAddon,
+  List,
+  ListProps,
+  Select,
+  Skeleton,
+  Text,
+  Tooltip,
+  useBoolean,
+  VStack
+} from '@chakra-ui/react';
+import { clamp } from '../shared/utils';
+import { CSNextIcon, CSPreviousIcon, CSSearchUser } from '../icons/CSIcons';
+import { InputChangeEvent } from '../shared/typeAlias';
+import { ReactNode, useMemo, useState } from 'react';
+import { WarningTwoIcon } from '@chakra-ui/icons';
 
 export type DataListItem = typeof DataListItem
 
@@ -26,27 +40,48 @@ export interface DataListProps<T> extends Omit<ListProps, 'children'> {
   error?: boolean
   searchValue: string
   options?: DataListOptions
+  sortable?: boolean
+  sortAttribute?: keyof T
   onSearch: (e: InputChangeEvent) => void
   onFilter?: (value: T) => boolean
   children: (data: T) => React.ReactElement<DataListItem>
 }
 
-const DataList = <T,>({ list, isLoading, error, searchValue, options, onFilter, onSearch, children, ...props }: DataListProps<T>) => {
+const DataList = <T,>({
+  list,
+  isLoading,
+  error,
+  searchValue,
+  options,
+  sortable,
+  sortAttribute,
+  onFilter,
+  onSearch,
+  children,
+  ...props
+}: DataListProps<T>) => {
   const [page, setPage] = useState(1)
-  const [pageSize, setPageSize] = useState(10)  
-  const filteredData = useMemo(() => list?.filter(onFilter ?? ALWAYS_TRUE) ?? [], [list, onFilter])
-  const {users} = useAppContext()
+  const [pageSize, setPageSize] = useState(10)
+  const [isAsc, setIsAsc] = useBoolean(true)
+  const filteredData = useMemo(() => (
+    list
+      ?.sort(sortAttribute ? ((a, b) => (
+        isAsc ?
+          a[sortAttribute] > b[sortAttribute] ? 1 : a[sortAttribute] < b[sortAttribute] ? -1 : 0 :
+          b[sortAttribute] > a[sortAttribute] ? 1 : b[sortAttribute] < a[sortAttribute] ? -1 : 0
+      )) : undefined)
+      .filter(onFilter ?? ALWAYS_TRUE) ?? []
+  ), [list, isAsc, onFilter])
   const handlePageChange = {
     previous: () => page > 1 ? setPage(page - 1) : setPage(1),
     next: () => page < Math.ceil(filteredData.length / pageSize) ? setPage(page + 1) : setPage(Math.ceil(filteredData.length / pageSize)),
     set: (index: number) => () => setPage(index)
   }
 
-  console.log(Math.ceil(filteredData.length / pageSize))
   if (error) {
     return (
       <HStack fontSize='2xl'>
-        <WarningTwoIcon/>
+        <WarningTwoIcon />
         <Text>Error</Text>
       </HStack>
     )
@@ -65,6 +100,23 @@ const DataList = <T,>({ list, isLoading, error, searchValue, options, onFilter, 
             </InputLeftAddon>
             <Input placeholder={options?.placeholder ?? "Buscar usuario"} value={searchValue} onChange={onSearch} />
           </InputGroup>
+          {
+            sortable &&
+            <Tooltip
+              hasArrow
+              placement='top'
+              label={isAsc ? "Descendente" : "Ascendente"}>
+              <IconButton
+                fontSize="2xl"
+                icon={
+                  <Icon as={
+                    isAsc ?
+                      AiOutlineSortAscending :
+                      AiOutlineSortDescending
+                  } />
+                } aria-label='Ordenar' onClick={setIsAsc.toggle} />
+            </Tooltip>
+          }
           <Select w='24' value={pageSize} onChange={({ target: { value } }) => {
             setPageSize(Number(value))
             setPage(clamp(page, 1, Math.ceil(filteredData.length / Number(value))))
@@ -78,25 +130,27 @@ const DataList = <T,>({ list, isLoading, error, searchValue, options, onFilter, 
         </HStack>
         <List {...props} rounded="md" border={BORDER}>
           {
-            filteredData.slice((page * pageSize) - pageSize, page * pageSize).map((e, i, a) =>
-              <Flex key={i} borderBottom={i === a.length - 1 ? "none" : BORDER}>
-                {children(e)}
-              </Flex>)
+            filteredData
+              .slice((page * pageSize) - pageSize, page * pageSize)
+              .map((e, i, a) =>
+                <Flex key={i} borderBottom={i === a.length - 1 ? "none" : BORDER}>
+                  {children(e)}
+                </Flex>)
           }
         </List>
-        <ButtonGroup isAttached variant="outline" style={{alignSelf: "center"}}>
+        <ButtonGroup isAttached variant="outline" style={{ alignSelf: "center" }}>
           <IconButton icon={<CSPreviousIcon />} aria-label='previous' onClick={handlePageChange.previous} isDisabled={page === 1} />
           {
             Array(Math.ceil(10))
               .fill('')
-              .map((_e, i) => ( 
-                  <Button key={i} isDisabled={i +1 +(page -5) > Math.ceil(filteredData.length / pageSize) || i +1 +(page -5) <=0} bg={i +1 +(page-5) === page ? "gray.200" : "white"} onClick={handlePageChange.set(i +1 +(page-5))}> {(i +1 +(page-5)) > 0  && (i +1 +(page-5)) <= Math.ceil((filteredData.length / pageSize)) ? i +1 +(page-5) : null }</Button>
+              .map((_e, i) => (
+                <Button key={i} isDisabled={i + 1 + (page - 5) > Math.ceil(filteredData.length / pageSize) || i + 1 + (page - 5) <= 0} bg={i + 1 + (page - 5) === page ? "gray.200" : "white"} onClick={handlePageChange.set(i + 1 + (page - 5))}> {(i + 1 + (page - 5)) > 0 && (i + 1 + (page - 5)) <= Math.ceil((filteredData.length / pageSize)) ? i + 1 + (page - 5) : null}</Button>
               ))
           }
           <IconButton icon={<CSNextIcon />} aria-label='next' onClick={handlePageChange.next} isDisabled={page === Math.ceil(filteredData.length / pageSize)} />
         </ButtonGroup>
       </VStack >
-      )
+  )
 }
 
 export default DataList
