c97593d388eadb650363117830b7ae32f994e845
daniel.ibarra <daniel.ibarra@proservicio.com>
2023-10-11 15:51:03 -0600

    test 2 organizacion

--- src/components/users/ImportUsersView.tsx
+++ src/components/users/ImportUsersView.tsx
@@ -14,6 +14,7 @@ import {
 import { SiMicrosoftexcel } from 'react-icons/si';
 import { useEffect, useState } from 'react';
 
+
 function readXLSX(file: File): Promise<any[]> {
   return new Promise((resolve, reject) => {
     const lector = new FileReader();
